ebdfb88fb591e32f12953feca0154a42eb9cdcd5
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-03 15:05:25 +0300

    Modified constants file

--- Context/constants.js
+++ Context/constants.js
@@ -1,6 +1,6 @@
-// import crowdFunding from "./CrowdFunding.json"
+import crowdFunding from "./CrowdFunding.json"
 
-// //Crowd Funding Marketplace
+//Crowd Funding Marketplace
 
-// export const CrowdFundingAddress = "0xmmdmmsmfmmaaaaaasfmffmdmss"
-// export const CrowdFundingABI = crowdFunding.abi
+export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const CrowdFundingABI = crowdFunding.abi
