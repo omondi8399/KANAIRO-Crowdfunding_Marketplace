ffa1fbffb75d58b053acb6989f5e52738a0f0d2d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-03 15:03:53 +0300

    Modified PopUp file

--- Components/PopUp.jsx
+++ Components/PopUp.jsx
@@ -33,15 +33,16 @@ const PopUp = ({ setOpenModel, donate, donateFunction, getDonations }) => {
       {/*header*/}
       <div className='flex items-start justify-between p-5 border-b border-solid
       border-slate-200 rounded-t'>
+
         <h3 className='text-3xl font-semibold'>{donate.title}</h3>
         <button className='p-1 ml-auto bg-transparent border-0 text-black opacity-5
-        float-right text-3xl leading-none font-semibold outline-none'
-        focus:outline-none
+        float-right text-3xl leading-none font-semibold outline-none
+        focus:outline-none'
         onClick={() => setOpenModel(false)}
         >
         <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl
         block outline-none focus:outline-none'>
-          x
+          X
         </span>
         </button>
       </div>
